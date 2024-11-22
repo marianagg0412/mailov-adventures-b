a3c6fb8c0cccdd613d2b184e8d414b069e77061b
santy-estrada <santiago.estrada6@eia.edu.co>
2024-11-21 20:58:40 -0500

    Port

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,6 @@ async function bootstrap() {
   app.enableCors({
     origin: configService.get('FRONT_URL'), 
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
